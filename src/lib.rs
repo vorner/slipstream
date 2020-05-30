f43a34932f6a817c0664fce0a7468d7869e787be
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-05-30 11:07:20 +0200

    Simplify iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 #![allow(non_camel_case_types)]
 
 use core::iter;
-use core::marker::PhantomData;
-use core::mem;
 use core::ops::*;
 
 use generic_array::{ArrayLength, GenericArray};
@@ -28,78 +26,6 @@ mod inner {
     unsafe impl Repr<u8> for Wrapping<u8> { }
     unsafe impl Repr<u16> for Wrapping<u16> { }
     unsafe impl Repr<u32> for Wrapping<u32> { }
-}
-
-// XXX Actually use these (and maybe hide them with impl Iterator)
-#[derive(Copy, Clone, Debug)]
-pub struct Iter<'a, V, B> {
-    data: &'a [B],
-    _v: PhantomData<V>,
-}
-
-impl<V, B> Iterator for Iter<'_, V, B>
-where
-    V: Vector<B> + Default,
-    B: Copy,
-{
-    type Item = V;
-    #[inline]
-    fn next(&mut self) -> Option<V> {
-        if self.data.len() >= V::LANES {
-            let (start, rest) = self.data.split_at(V::LANES);
-            self.data = rest;
-            Some(V::new(start))
-        } else if self.data.is_empty() {
-            None
-        } else {
-            let mut vec = V::default();
-            vec.deref_mut()[..self.data.len()].copy_from_slice(self.data);
-            self.data = &[];
-            Some(vec)
-        }
-    }
-
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = (self.data.len() + V::LANES - 1) / V::LANES;
-        (len, Some(len))
-    }
-}
-
-struct IterMut<'a, V, B> {
-    data: &'a mut [B],
-    _v: PhantomData<V>,
-}
-
-impl<'a, V, B> Iterator for IterMut<'a, V, B>
-where
-    V: Vector<B> + Default,
-    B: Copy,
-{
-    type Item = MutProxy<'a, V, B>;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.data.len() >= V::LANES {
-            let data = mem::take(&mut self.data);
-            let (start, rest) = data.split_at_mut(V::LANES);
-            self.data = rest;
-            let data = V::new(start);
-            Some(MutProxy {
-                data,
-                restore: start,
-            })
-        } else if self.data.is_empty() {
-            None
-        } else {
-            let mut data = V::default();
-            let restore = mem::take(&mut self.data);
-            data.deref_mut()[..self.data.len()].copy_from_slice(self.data);
-            Some(MutProxy {
-                data,
-                restore,
-            })
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -165,28 +91,86 @@ pub trait Vector<B>: Deref<Target = [B]> + DerefMut + Sized + 'static {
 }
 
 #[inline]
-pub fn vectorize<'a, B, V>(data: &'a [B]) -> impl Iterator<Item = V> + 'a
+pub fn vectorize<'a, V, B>(data: &'a [B], mut empty: V) -> impl Iterator<Item = V> + 'a
 where
     B: Copy,
     V: Vector<B> + Default,
 {
-    Iter {
-        data,
-        _v: PhantomData,
-    }
+    let chunks = data.chunks_exact(V::LANES);
+    let last = chunks.remainder();
+    let last = if last.is_empty() {
+        None
+    } else {
+        empty[0..last.len()].copy_from_slice(last);
+        Some(empty)
+    };
+    chunks.map(V::new).chain(last)
 }
 
 #[inline]
-pub fn vectorize_mut<'a, B, V>(data: &'a mut [B])
+pub fn vectorize_exact<'a, V, B>(data: &'a [B]) -> impl Iterator<Item = V> + 'a
+where
+    B: Copy,
+    V: Vector<B> + Default,
+{
+    assert!(
+        data.len() % V::LANES == 0,
+        "Data to vectorize_exact must be divisible by number of lanes ({} % {})",
+        data.len(), V::LANES,
+    );
+    data.chunks_exact(V::LANES).map(V::new)
+}
+
+#[inline]
+pub fn vectorize_mut<'a, V, B>(mut data: &'a mut [B], mut empty: V)
     -> impl Iterator<Item = impl DerefMut<Target = V> + 'a> + 'a
 where
     B: Copy,
     V: Vector<B> + Default,
 {
-    IterMut {
-        data,
-        _v: PhantomData,
+    let rem = data.len() % V::LANES;
+    let mut last = None;
+    if rem > 0 {
+        let (d, r) = data.split_at_mut(data.len() - rem);
+        data = d;
+        empty[0..rem].copy_from_slice(r);
+        last = Some(MutProxy {
+            data: empty,
+            restore: r,
+        });
     }
+
+    vectorize_mut_inner(data).chain(last)
+}
+
+#[inline]
+fn vectorize_mut_inner<'a, V, B>(data: &'a mut [B])
+    -> impl Iterator<Item = MutProxy<'a, V, B>> + 'a
+where
+    B: Copy,
+    V: Vector<B> + Default,
+{
+    data
+        .chunks_exact_mut(V::LANES)
+        .map(|d| MutProxy {
+            data: V::new(d),
+            restore: d,
+        })
+}
+
+#[inline]
+pub fn vectorize_mut_exact<'a, V, B>(data: &'a mut [B])
+    -> impl Iterator<Item = impl DerefMut<Target = V> + 'a> + 'a
+where
+    B: Copy,
+    V: Vector<B> + Default,
+{
+    assert!(
+        data.len() % V::LANES == 0,
+        "Data to vectorize_exact must be divisible by number of lanes ({} % {})",
+        data.len(), V::LANES,
+    );
+    vectorize_mut_inner(data)
 }
 
 #[cfg(test)]
@@ -196,7 +180,7 @@ mod tests {
     #[test]
     fn iter() {
         let data = (0..=10u16).collect::<Vec<_>>();
-        let vtotal = vectorize(&data)
+        let vtotal = vectorize(&data, u16x8::default())
             .fold(u16x8::default(), |a, b| a + b);
         let total: u16 = vtotal.iter().sum();
         assert_eq!(total, 55);
@@ -207,7 +191,7 @@ mod tests {
         let data = (0..33u32).collect::<Vec<_>>();
         let mut dst = [0u32; 33];
         let ones = u32x4::splat(1);
-        for (mut d, s) in vectorize_mut(&mut dst).zip(vectorize(&data)) {
+        for (mut d, s) in vectorize_mut(&mut dst, u32x4::default()).zip(vectorize(&data, u32x4::default())) {
             *d = ones + s;
         }
 

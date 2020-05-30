ef80bf7023423104329ca23063d3e62b562a5dfa
Michal 'vorner' Vaner <vorner@vorner.cz>
2020-05-30 22:23:05 +0200

    Some basic benchmarks

---
+++ benches/sum.rs
@@ -0,0 +1,90 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::iter;
+
+use impatient::prelude::*;
+use multiversion::multiversion;
+use test::Bencher;
+
+const SIZE: usize = 10*1024*1024;
+type V = impatient::f32x16;
+
+#[bench]
+fn basic(b: &mut Bencher) {
+    let data: Vec<f32> = iter::repeat_with(rand::random)
+        .take(SIZE)
+        .collect();
+
+    b.iter(|| {
+        test::black_box(data.iter().sum::<f32>());
+    })
+}
+
+#[multiversion]
+#[clone(target = "[x86|x86_64]+sse+sse2+sse3+sse4.1+avx+avx2")]
+#[clone(target = "[x86|x86_64]+sse+sse2+sse3+sse4.1+avx")]
+#[clone(target = "[x86|x86_64]+sse+sse2+sse3+sse4.1")]
+fn vectorized(data: &[V]) -> f32 {
+    let mut result = V::default();
+
+    for v in data {
+        result += *v;
+    }
+
+    result.iter().sum()
+}
+
+#[bench]
+fn vectorized_default(b: &mut Bencher) {
+    let data: Vec<V> = iter::repeat_with(rand::random)
+        .take(SIZE / V::LANES)
+        .map(|v: [f32; V::LANES]| V::new(&v))
+        .collect();
+
+    b.iter(|| {
+        test::black_box(vectorized_default_version(&data));
+    })
+}
+
+#[bench]
+fn vectorized_detect(b: &mut Bencher) {
+    let data: Vec<V> = iter::repeat_with(rand::random)
+        .take(SIZE / V::LANES)
+        .map(|v: [f32; V::LANES]| V::new(&v))
+        .collect();
+
+    b.iter(|| {
+        test::black_box(vectorized(&data));
+    })
+}
+
+#[bench]
+#[cfg(target_arch = "x86_64")]
+fn vectorized_manual_sse(b: &mut Bencher) {
+    use core::arch::x86_64::{self as arch, __m128};
+    use core::mem;
+
+    let data: Vec<__m128>;
+    unsafe {
+        data = iter::repeat_with(|| {
+                let v: [f32; 4] = rand::random();
+                arch::_mm_loadu_ps(v.as_ptr())
+            })
+            .take(SIZE / 4)
+            .collect();
+    }
+
+    b.iter(|| {
+        unsafe {
+            let mut result = arch::_mm_setzero_ps();
+            for v in &data {
+                result = arch::_mm_add_ps(result, *v);
+            }
+
+            let result: [f32; 4] = mem::transmute(result);
+            test::black_box(result.iter().sum::<f32>());
+        }
+    })
+}
